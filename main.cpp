2da5a05b3d464ed9b9d4b88d5f9f08802adee63d
SodaCris <18463922396@163.com>
2021-10-21 21:30:16 +0800

    test caching

--- main.cpp
+++ main.cpp
@@ -13,5 +13,6 @@ TEST_CASE("Factorials are computed", "[factorial]")
 	REQUIRE(Factorial(1) == 1);
 	REQUIRE(Factorial(2) == 2);
 	REQUIRE(Factorial(3) == 6);
+	REQUIRE(Factorial(4) == 48);
 	REQUIRE(Factorial(10) == 3628800);
 }
