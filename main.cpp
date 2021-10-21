ff62b929e03264a8fab04b4bdd92a0024acb7a14
SodaCris <18463922396@163.com>
2021-10-21 21:13:29 +0800

    testing cache

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 unsigned int Factorial(unsigned int number)
 {
-	//return number > 1 ? Factorial(number - 1) * number : 1;
-	return number <= 1 ? number : Factorial(number - 1) * number;
+	return number > 1 ? Factorial(number - 1) * number : 1;
+	// return number <= 1 ? number : Factorial(number - 1) * number;
 }
 
 TEST_CASE("Factorials are computed", "[factorial]")
