b6c9cd4fcb6d24e5a36e88d4ba24bcccdfe890e8
SodaCris <18463922396@163.com>
2021-10-21 19:08:50 +0800

    testing falied CTest

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 
 unsigned int Factorial(unsigned int number)
 {
-    return number > 1 ? Factorial(number - 1) * number : 1;
+	//return number > 1 ? Factorial(number - 1) * number : 1;
+	return number <= 1 ? number : Factorial(number - 1) * number;
 }
 
 TEST_CASE("Factorials are computed", "[factorial]")
 {
-    REQUIRE(Factorial(0) == 1);
-    REQUIRE(Factorial(1) == 1);
-    REQUIRE(Factorial(2) == 2);
-    REQUIRE(Factorial(3) == 6);
-    REQUIRE(Factorial(10) == 3628800);
+	REQUIRE(Factorial(0) == 1);
+	REQUIRE(Factorial(1) == 1);
+	REQUIRE(Factorial(2) == 2);
+	REQUIRE(Factorial(3) == 6);
+	REQUIRE(Factorial(10) == 3628800);
 }
